65ccb490aa1af8234937991fb9f10fd89b71463d
Michaelbiocchi <michael.biocchi@mail.utoronto.ca>
2026-08-09 12:03:32 -0400

    Adding this to the code in order for us to see the plot I would still recomend we find data to scatter to see if it exhibits a similar pattern

--- simulation_1.py
+++ simulation_1.py
@@ -1,5 +1,6 @@
 # We will assume a road has n lanes, vehicles are all identical in size and can only travel
 # in one direction, recall before we begin theat density equals flow/speed
+import matplotlib.pyplot as plt
 
 # First consider the constant outflow model 
 
@@ -7,14 +8,30 @@
 # conditons and see how they compare to a similar model.
 
 # 1)
-def run_simulation_1(initial_vehicles: float, inflow: int, outflow: int):
 
+def run_simulation_1(initial_vehicles: float, inflow: int, outflow: int):
+    time = []
+    number_of_vehicles = []
     for t in range(13):
+        time.append(t)
+        N = (inflow-outflow) * t + initial_vehicles
+        number_of_vehicles.append(N)
         print (f"time = {t*5} min, Number of vehicles = {(inflow-outflow) * t + initial_vehicles}")
 
+    return time, number_of_vehicles
 
 
-run_simulation_1(81,482,482)
+
+time, number_of_vehicles = run_simulation_1(81, 482, 489)
+
+plt.plot(time, number_of_vehicles, 'r-', label="Lane 1")
+plt.xlabel('Time measured in 5 minute increments')
+plt.ylabel('Cars in Lanes')
+plt.legend(loc='best')
+plt.title("How Many Cars Enter and Leave The Lane")
+plt.tight_layout()
+plt.show()
+
 
 # For an example consider the road segment Mainline VDS 717404  to Mainline VDS 716295
 # this has length approximately 0.9 miles we know the flow and avg speed of the upstream detector is 482 * 12 = 5784 vehicles per hour
--- simulation_2.py
+++ simulation_2.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 # So this will be our Linear outflow model where A is a constant function, B is proportional to the number of vehicles on
 # the road (i.e. B(N) = kN(t))
 
@@ -7,8 +8,24 @@ import math
 def run_simulation_2(initial_vehicles: float, A: float, k: float) :
     # C = initial vehicles - A/k
     C = initial_vehicles - A/k
+    time = []
+    vehicles = []
 
     for t in range(13):
+        N = C*math.exp(-k*t) + A/k
+        time.append(t)
+        vehicles.append(N)
         print(f"time = {t*5} min, vehicles = {C*math.exp(-k*t) + A/k}")
 
+    return time, vehicles
+
 # run_simulation_2()
+time, vehicles = run_simulation_2(81, 64, 1.5)
+
+plt.plot(time, vehicles, 'r-', label="Lane 1")
+plt.xlabel('Time measured in 5 minute increments')
+plt.ylabel('Cars in Lanes')
+plt.legend(loc='best')
+plt.title("How Many Cars Enter and Leave The Lane")
+plt.tight_layout()
+plt.show()
